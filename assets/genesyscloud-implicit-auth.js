3c06b5675159f3db4e43de0f42a513a9c1f44138
mario-genesys <123017929+mario-genesys@users.noreply.github.com>
2025-08-06 09:45:43 -0600

    Add files via upload

---
+++ assets/genesyscloud-implicit-auth.js
@@ -0,0 +1,46 @@
+const urlParameters = {};
+
+function getParameterByName(name) {
+  const escapedName = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+  const regex = new RegExp(`[\\#&]${escapedName}=([^&#]*)`);
+  const results = regex.exec(window.location.hash);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function retrieveUrlParameters(state) {
+  if (state === undefined || state.length === 0) {
+    return;
+  }
+  decodeURIComponent(state).replace('?', '').split('&').forEach((val) => {
+    const parameter = val.split('=', 2);
+    urlParameters[parameter[0]] = parameter[1] === undefined
+      ? true
+      : decodeURIComponent(parameter[1]);
+  });
+}
+
+if (window.location.hash) {
+  const accessToken = getParameterByName('access_token');
+  const state = getParameterByName('state');
+  retrieveUrlParameters(state);
+  window.urlParameters = urlParameters;
+  window.accessToken = accessToken; 
+  if (urlParameters.env) {
+    const environment = window.platformClient.PureCloudRegionHosts[urlParameters.env];
+    if (environment) {
+      window.platformClient.ApiClient.instance.setEnvironment(environment);
+    }
+  }
+} else {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const clientId = urlSearchParams.get('clientId');
+  const queryStringData = {
+    response_type: 'token',
+    client_id: clientId,
+    redirect_uri: [window.location.protocol, '//', window.location.host, window.location.pathname].join(''),
+    state: window.location.search,
+  };
+  const queryStringElements = Object.keys(queryStringData).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(queryStringData[key])}`);
+  const queryString = queryStringElements.join('&');
+  window.location.replace(`https://login.mypurecloud.com/oauth/authorize?${queryString}`);
+}
